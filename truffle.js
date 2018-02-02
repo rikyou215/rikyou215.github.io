0b669e2408c7733f22f0ccfa2ddaaeb6a5488d28
frank <frankli@pgyer.com>
2018-02-02 14:28:08 +0800

    Site updated: 2018-02-02 14:28:08

--- truffle.js
+++ truffle.js
@@ -1,5 +1,5 @@
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
+  // See <http: truffleframework.com="" docs="" advanced="" configuration="">
   // to customize your Truffle configuration!
   networks: {
     development: {
@@ -9,3 +9,4 @@ module.exports = {
     }
   }
 };
+</http:>
